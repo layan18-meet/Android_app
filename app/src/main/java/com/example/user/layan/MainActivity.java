e4cd4129b47ad7d3ffd1cdef0f57874b8d6b2e30
user <mlayan147@gmail.com>
2018-09-21 09:49:29 +0300

    "Option menu"

--- app/src/main/java/com/example/user/layan/MainActivity.java
+++ app/src/main/java/com/example/user/layan/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.user.layan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button b1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.b1);
+        b1.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Intent i;
+        if (v == b1) {
+            i = new Intent(this, Main2Activity.class);
+            startActivity(i);
+        }
+
     }
 }
