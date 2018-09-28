f23c05443d18bb5e924585ef148c57fa7b78d4b3
layan18-meet <mlayan147@gmail.com>
2018-09-28 09:54:16 +0300

    "Added alertDialog, and signUpActivity"

--- app/src/main/java/com/example/user/layan/signUpActivity.java
+++ app/src/main/java/com/example/user/layan/signUpActivity.java
@@ -1,10 +1,12 @@
 package com.example.user.layan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class signUpActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,10 +24,32 @@ public class signUpActivity extends AppCompatActivity implements View.OnClickLis
         phone_number= (EditText) findViewById(R.id.phone_number);
     }
 
+    boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    String getEmail = email.getText().toString();
+
+
 
     @Override
     public void onClick(View v) {
         if ( v==signUp ) {
+            if ((username.equals("") || password==null || email.equals("") || phone_number==null) || (!isEmailValid(getEmail))) {
+
+                //Check if one or more fields are empty
+                if (username.equals("") || password==null || email.equals("") || phone_number==null)
+                    Toast.makeText(this, "There Are Empty Fields", Toast.LENGTH_SHORT).show();
+
+                // Check if email id is valid or not
+                if (!isEmailValid(getEmail))
+                    Toast.makeText(this, "\"Your Email Id is Invalid.\"", Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                Intent i = new Intent(this, homeActivity.class);
+                startActivity(i);
+            }
 
         }
     }
